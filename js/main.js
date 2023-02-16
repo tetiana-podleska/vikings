dadc0e1467b213be3bf8cf68cb1785c5f2da9019
Tetiana <podleskatetiana@gmail.com>
2023-02-16 19:02:00 +0200

    counter reset li

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
-$(function(){
-
+$(function () {
+  $("[data-fancybox]").fancybox({
+    youtube: {
+      controls: 0,
+      showinfo: 0,
+    },
+    vimeo: {
+      color: "f00",
+    },
+  });
 });
