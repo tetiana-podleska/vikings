c9ebb6c0d0fc23a185da6288971e1a778f6bb606
Tetiana <podleskatetiana@gmail.com>
2023-02-17 21:52:41 +0200

    finished main page / started header on page season 1

--- js/main.js
+++ js/main.js
@@ -8,4 +8,23 @@ $(function () {
       color: "f00",
     },
   });
+
+  $(".heroes__slider-img").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: ".heroes__slider-text",
+    prevArrow: `<button class="slick-btn slick-prev">
+    <img src="images/arrow-prev.png" alt="prev" />
+  </button>`,
+    nextArrow: `<button class="slick-btn slick-next">
+  <img src="images/arrow-next.png" alt="next" />
+</button>`,
+  });
+  $(".heroes__slider-text").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: ".heroes__slider-img",
+  });
 });
