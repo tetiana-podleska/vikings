33f3ee8ea8c7593f671ae11485645adb8aeced17
Tetiana <podleskatetiana@gmail.com>
2023-02-19 22:11:39 +0200

    finished media queries

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 $(function () {
+  $(".menu__btn").on("click", function () {
+    $(".menu__list").toggleClass("menu__list--active");
+  });
+
   $(".heroes__slider-img").slick({
     slidesToShow: 1,
     slidesToScroll: 1,
@@ -9,6 +13,14 @@ $(function () {
     nextArrow: `<button class="slick-btn slick-next">
   <img src="images/arrow-next.png" alt="next" />
 </button>`,
+    responsive: [
+      {
+        breakpoint: 769,
+        settings: {
+          arrows: false,
+        },
+      },
+    ],
   });
   $(".heroes__slider-text").slick({
     slidesToShow: 1,
